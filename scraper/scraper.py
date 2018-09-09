4159c2204570d596474857d9aaa2115791da125a
denibulkashvili <denibulkashvili@gmail.com>
2018-09-09 21:14:58 +0800

    Finish the scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,10 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+import datetime
 
-source = requests.get('https://codepen.io/').text
+
+indeed_home = "https://th.indeed.com/"
+search_query = "jobs?q=Python+Developer&sort=date"
+source = requests.get(indeed_home+search_query).text
 soup = BeautifulSoup(source, 'lxml')
 
-pen = soup.find('div', class_="single-pen")
- 
+now =  datetime.datetime.now()
+date = now.strftime("%Y-%m-%d")
+fname = date+".csv"
 
-print(title)
+csv_file = open(fname, 'w', encoding="utf-8")
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['title', 'company', 'location', 'summary', 'link'])
+
+
+jobs = soup.find_all('div', class_="result") 
+
+for job in jobs:
+    title = job.h2.a.text
+    company = job.div.span.text.lstrip()
+    location = job.find('span', class_="location").text
+    summary = job.find('span', class_="summary").text.lstrip()
+    link = indeed_home + job.h2.a['href']
+
+    csv_writer.writerow([title, company, location, summary, link])
+
+csv_file.close()
